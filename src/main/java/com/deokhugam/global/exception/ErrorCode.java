b498a8739612f6fef24e46b9a8319dc4cd852079
sungwoong-svg <skarn506@gmail.com>
2026-08-13 17:21:33 +0900

    feat: 도서 CRUD 및 목록 조회 기능 구현 (#3)
    * feat: 도서 등록 및 단건 조회 기능 구현
    
    * chore: CI PostgreSQL 테스트 환경 구성
    
    * feat: 도서 목록 검색 및 정렬 기능 구현
    
    * feat: 도서 목록 커서 페이지네이션 구현
    
    * feat: 도서 수정 기능 구현
    
    * feat: 도서 소프트 삭제 기능 구현
    
    * chore: PR 템플릿 추가

--- src/main/java/com/deokhugam/global/exception/ErrorCode.java
+++ src/main/java/com/deokhugam/global/exception/ErrorCode.java
@@ -4,6 +4,16 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
+  DUPLICATE_BOOK(
+      HttpStatus.CONFLICT,
+      "이미 등록된 ISBN입니다."
+  ),
+
+  BOOK_NOT_FOUND(
+      HttpStatus.NOT_FOUND,
+      "도서를 찾을 수 없습니다."
+  ),
+
   INTERNAL_SERVER_ERROR(
       HttpStatus.INTERNAL_SERVER_ERROR,
       "서버 내부 오류가 발생했습니다."
