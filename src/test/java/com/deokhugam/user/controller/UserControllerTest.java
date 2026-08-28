67f445b2d3a5367e0bdd4c147157b9228720f8de
ssummer96 <pupu25866@gmail.com>
2026-08-28 17:35:44 +0900

    fix:UserControllerTest Mock->MockBean으로 수정

--- src/test/java/com/deokhugam/user/controller/UserControllerTest.java
+++ src/test/java/com/deokhugam/user/controller/UserControllerTest.java
@@ -8,9 +8,9 @@ import com.deokhugam.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,10 +33,10 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Mock
+    @MockBean
     private UserService userService;
 
-    @Mock
+    @MockBean
     private DashboardQueryService dashboardQueryService;
 
     @Test
